dfb8aa42a9ef2af649d331860acbd81a1880382c
Claudio Rosati <claudio.rosati@esss.se>
2016-09-07 17:21:29 -0400

    Improve build script

--- clean-and-build.sh
+++ clean-and-build.sh
@@ -11,41 +11,42 @@ cd ..
 # or want to assert that you start over fresh,
 # delete the Maven repository:
 rm -rf $HOME/.m2/repository
+rm -f ?_*.log
 
 echo "===="
 echo "==== BUILDING maven-osgi-bundles"
 echo "===="
-(cd maven-osgi-bundles; mvn clean verify)
+(cd maven-osgi-bundles; time mvn clean verify) | tee 0_maven-osgi-bundles.log
 
 echo "===="
 echo "==== BUILDING cs-studio-thirdparty"
 echo "===="
-(cd cs-studio-thirdparty; mvn clean verify)
+(cd cs-studio-thirdparty; time mvn clean verify) | tee 1_cs-studio-thirdparty.log
 
 echo "===="
 echo "==== BUILDING diirt"
 echo "===="
-(cd diirt; mvn clean verify)
+(cd diirt; time mvn clean verify) | tee 2_diirt.log
 
 echo "===="
 echo "==== BUILDING cs-studio/core"
 echo "===="
-(cd cs-studio/core; mvn clean verify)
+(cd cs-studio/core; time mvn clean verify) | tee 3_cs-studio-core.log
 
 echo "===="
 echo "==== BUILDING cs-studio/applications"
 echo "===="
-(cd cs-studio/applications; mvn clean verify)
+(cd cs-studio/applications; time mvn clean verify) | tee 4_cs-studio-applications.log
 
 echo "===="
 echo "==== BUILDING org.csstudio.product"
 echo "===="
-(cd org.csstudio.product; mvn clean verify)
+(cd org.csstudio.product; time mvn clean verify) | tee 5_org.csstudio.product.log
 
 echo "===="
 echo "==== BUILDING org.csstudio.ess.product"
 echo "===="
-(cd org.csstudio.ess.product; mvn clean verify)
+(cd org.csstudio.ess.product; time mvn clean verify) | tee 6_org.csstudio.ess.product.log
 
 # Displaying execution time
 DUR=$(echo "$(date +%s) - $START" | bc)
@@ -54,4 +55,3 @@ SDUR=`expr $DUR - 60 \* $MDUR`; \
 echo "===="
 echo "==== Building took $MDUR minutes and $SDUR seconds."
 echo "===="
-
