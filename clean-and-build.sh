024a9948d695c1c1741f68bcd0d4e7648d4d7f1a
Claudio Rosati <claudio.rosati@esss.se>
2016-09-19 07:07:55 -0400

    Removed unuseful statements

--- clean-and-build.sh
+++ clean-and-build.sh
@@ -11,8 +11,6 @@ cd ..
 # or want to assert that you start over fresh,
 # delete the Maven repository:
 # rm -rf $HOME/.m2/repository
-rm -rf $HOME/.m2/repository/.cache
-rm -rf $HOME/.m2/repository/.meta
 rm -rf $HOME/.m2/repository/p2/bundle/osgi/org.csstudio.*
 rm -rf $HOME/.m2/repository/p2/bundle/osgi/org.diirt.*
 rm -f ?_*.log
@@ -20,47 +18,57 @@ rm -f ?_*.log
 # To reduce maven verbosity
 # MAVEN_OPTS = $MAVEN_OPTS -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn
 
+echo ""
 echo "===="
 echo "==== BUILDING maven-osgi-bundles"
 echo "===="
 (cd maven-osgi-bundles; time mvn -B clean verify) | tee 0_maven-osgi-bundles.log
 
+echo ""
 echo "===="
 echo "==== BUILDING cs-studio-thirdparty"
 echo "===="
 (cd cs-studio-thirdparty; time mvn -B clean verify) | tee 1_cs-studio-thirdparty.log
 
+echo ""
 echo "===="
 echo "==== BUILDING diirt"
 echo "===="
 (cd diirt; time mvn -B clean verify) | tee 2_diirt.log
 
+echo ""
 echo "===="
 echo "==== BUILDING cs-studio/core"
 echo "===="
 (cd cs-studio/core; time mvn -B clean clean verify) | tee 3_cs-studio-core.log
 
+echo ""
 echo "===="
 echo "==== BUILDING cs-studio/applications"
 echo "===="
 (cd cs-studio/applications; time mvn -B clean clean verify) | tee 4_cs-studio-applications.log
 
+echo ""
 echo "===="
-echo "==== BUILDING cs-studio/applications"
+echo "==== BUILDING org.csstudio.display.builder"
 echo "===="
 (cd org.csstudio.display.builder; time mvn -Dcss-repo=file:/Users/claudiorosati/Projects/GitHub/ess-css-extra/ess_css_comp_repo -Declipse-site=http://download.eclipse.org/releases/mars -B clean verify) | tee 5_org.csstudio.display.builder.log
 
+echo ""
 echo "===="
 echo "==== BUILDING org.csstudio.product"
 echo "===="
 (cd org.csstudio.product; time mvn -B clean verify) | tee 6_org.csstudio.product.log
 
+echo ""
 echo "===="
 echo "==== BUILDING org.csstudio.ess.product"
 echo "===="
 (cd org.csstudio.ess.product; time mvn -B clean verify) | tee 7_org.csstudio.ess.product.log
 
+echo ""
 tail ?_*.log
+echo ""
 
 # Displaying execution time
 DUR=$(echo "$(date +%s) - $START" | bc)
