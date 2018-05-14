43cc7131e1bbafcfec543084ff3f198f40f01903
Claudio Rosati <Claudio.Rosati@esss.se>
2018-05-14 10:39:05 +0200

    Updated JDK.

--- clean-and-build-no-tests.sh
+++ clean-and-build-no-tests.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 
-export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.8.0_162.jdk/Contents/Home
+export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.8.0_172.jdk/Contents/Home
 
 echo ""
 echo "===="
